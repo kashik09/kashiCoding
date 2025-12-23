442f0d5cc9b9eb51e7d20f9d488efeeee47a0f91
kashik09 <kashiku789@gmail.com>
2025-12-23 15:26:36 +0300

    fix: set default data-theme on html

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en" suppressHydrationWarning>
+    <html lang="en" suppressHydrationWarning data-theme="dracula">
       <body className={inter.className}>
         <ThemeProvider>
           <ToastProvider>
