628118e7a4d6c2411eff7119150f7cf7b650ba6f
kashik09 <kashiku789@gmail.com>
2026-05-21 14:04:40 +0300

    purge: remove marketplace (products, cart, checkout, orders, licenses, wishlist)
    Delete digital product marketplace functionality:
    
    - Products pages and product detail
    
    - Cart and checkout flow
    
    - Order management (user and admin)
    
    - License system
    
    - Wishlist functionality
    
    - Payment providers
    
    - Related API routes, components, lib files
    
    - Pricing content

--- components/features/cart/index.ts
+++
@@ -1,2 +0,0 @@
-export { CartIcon } from './CartIcon'
-export { CartItem } from './CartItem'
--- components/features/shop/index.ts
+++
@@ -1,4 +0,0 @@
-export { ProductCard } from './ProductCard'
-export { ProductGrid } from './ProductGrid'
-export { PriceDisplay } from './PriceDisplay'
-export { LicenseSelector } from './LicenseSelector'
